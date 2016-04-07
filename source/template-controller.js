424abcdb28d8088916a411519d2f56fabf8443f8
Dominik Guzei <dominik.guzei@gmail.com>
2016-04-07 16:52:18 +0200

    introduces proper state and props management

--- source/template-controller.js
+++ source/template-controller.js
@@ -2,6 +2,10 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import ExtendableError from 'es6-error';
 
+const DEFAULT_API = [
+  'state', 'props', 'helpers', 'events', 'onCreated', 'onRendered', 'onDestroyed'
+];
+
 // Helpers
 const withTemplateInstanceContext = function(handler) {
   return function() {
@@ -16,6 +20,17 @@ const bindToTemplateInstance = function(handlers) {
   return handlers;
 };
 
+const generateReactiveAccessor = function(defaultValue) {
+  let value = new ReactiveVar(defaultValue);
+  return function(newValue) {
+    if (newValue !== undefined) {
+      value.set(newValue);
+    } else {
+      return value.get();
+    }
+  };
+};
+
 // Errors
 class TemplateNotFoundError extends ExtendableError {
   constructor(templateName) {
@@ -23,36 +38,74 @@ class TemplateNotFoundError extends ExtendableError {
   }
 }
 
-const TemplateController = {
+class PropertyValidatorRequired extends ExtendableError {
+  constructor() {
+    super('<data> must be a validator with #clean and #validate methods (see: SimpleSchema)');
+  }
+}
 
-  create(templateName, config) {
-    // Template reference
-    let template = Template[templateName];
-    if (!template) {
-      throw new TemplateNotFoundError(templateName);
-    }
-    let { state, helpers, events, onCreated, onRendered, onDestroyed} = config;
-    // State
-    if (state) {
-      template.onCreated(function() {
-        for (let key of Object.keys(state)) {
-          this[key] = new ReactiveVar(state[key]);
+const TemplateController = function(templateName, config) {
+  // Template reference
+  let template = Template[templateName];
+  if (!template) {
+    throw new TemplateNotFoundError(templateName);
+  }
+  let { state, props, helpers, events, onCreated, onRendered, onDestroyed } = config;
+
+  // Remove all standard api props fromt he config so we can have add the
+  // rest to the template instance!
+  for (apiProp of DEFAULT_API) {
+    delete config[apiProp];
+  }
+
+  // State & Custom instance methods
+  if (state) {
+    template.onCreated(function() {
+      this.state = {};
+      // Setup the state as reactive vars
+      for (let key of Object.keys(state)) {
+        this.state[key] = generateReactiveAccessor(state[key]);
+      }
+      // Add the custom helpers and properties to the template instance
+      for (let key of Object.keys(config)) {
+        this[key] = config[key];
+      }
+    });
+  }
+  // Default values for props
+  if (props) {
+    template.onCreated(function() {
+      this.props = {};
+      this.autorun(() => {
+        if (!props.validate) throw new PropertyValidatorRequired();
+        let currentData = Template.currentData() || {};
+        props.clean(currentData);
+        props.validate(currentData);
+        for (let key of Object.keys(currentData)) {
+          let value = currentData[key];
+          if (!this.props[key]) {
+            this.props[key] = generateReactiveAccessor(value);
+          } else {
+            this.props[key](value);
+          }
         }
       });
-    }
-    // Helpers
-    if (helpers) {
-      template.helpers(bindToTemplateInstance(helpers));
-    }
-    // Events
-    if (events) {
-      template.events(bindToTemplateInstance(events));
-    }
-    // Lifecycle
-    if (onCreated) template.onCreated(onCreated);
-    if (onRendered) template.onRendered(onRendered);
-    if (onDestroyed) template.onDestroyed(onDestroyed);
+    });
   }
+  // Helpers
+  if (helpers) {
+    helpers.state = function() { return this.state; };
+    helpers.props = function() { return this.props; };
+    template.helpers(bindToTemplateInstance(helpers));
+  }
+  // Events
+  if (events) {
+    template.events(bindToTemplateInstance(events));
+  }
+  // Lifecycle
+  if (onCreated) template.onCreated(onCreated);
+  if (onRendered) template.onRendered(onRendered);
+  if (onDestroyed) template.onDestroyed(onDestroyed);
 
 };
 
